2b9a71bb64a6b44989658abca93723e0e754462e
Luis Perugachi <luisprg05@gmail.com>
2023-05-20 21:17:35 -0500

    Creado controladores

---
+++ BlogPeliculas/BlogPeliculas.API/Controllers/ComentariosController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlogPeliculas.Modelos;
+
+namespace BlogPeliculas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentariosController : ControllerBase
+    {
+        private readonly BlogPeliculasContext _context;
+
+        public ComentariosController(BlogPeliculasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Comentarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comentario>>> GetComentario()
+        {
+          if (_context.Comentarios == null)
+          {
+              return NotFound();
+          }
+            return await _context.Comentarios.ToListAsync();
+        }
+
+        // GET: api/Comentarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Comentario>> GetComentario(int id)
+        {
+          if (_context.Comentarios == null)
+          {
+              return NotFound();
+          }
+            var comentario = await _context.Comentarios.FindAsync(id);
+
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+
+            return comentario;
+        }
+
+        // PUT: api/Comentarios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComentario(int id, Comentario comentario)
+        {
+            if (id != comentario.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(comentario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ComentarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Comentarios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Comentario>> PostComentario(Comentario comentario)
+        {
+          if (_context.Comentarios == null)
+          {
+              return Problem("Entity set 'BlogPeliculasContext.Comentario'  is null.");
+          }
+            _context.Comentarios.Add(comentario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetComentario", new { id = comentario.Id }, comentario);
+        }
+
+        // DELETE: api/Comentarios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteComentario(int id)
+        {
+            if (_context.Comentarios == null)
+            {
+                return NotFound();
+            }
+            var comentario = await _context.Comentarios.FindAsync(id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Comentarios.Remove(comentario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ComentarioExists(int id)
+        {
+            return (_context.Comentarios?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ BlogPeliculas/BlogPeliculas.API/Controllers/PersonajesController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlogPeliculas.Modelos;
+
+namespace BlogPeliculas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonajesController : ControllerBase
+    {
+        private readonly BlogPeliculasContext _context;
+
+        public PersonajesController(BlogPeliculasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Personajes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Personaje>>> GetPersonaje()
+        {
+          if (_context.Personajes == null)
+          {
+              return NotFound();
+          }
+            return await _context.Personajes.ToListAsync();
+        }
+
+        // GET: api/Personajes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Personaje>> GetPersonaje(int id)
+        {
+          if (_context.Personajes == null)
+          {
+              return NotFound();
+          }
+            var personaje = await _context.Personajes.FindAsync(id);
+
+            if (personaje == null)
+            {
+                return NotFound();
+            }
+
+            return personaje;
+        }
+
+        // PUT: api/Personajes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPersonaje(int id, Personaje personaje)
+        {
+            if (id != personaje.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(personaje).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonajeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Personajes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Personaje>> PostPersonaje(Personaje personaje)
+        {
+          if (_context.Personajes == null)
+          {
+              return Problem("Entity set 'BlogPeliculasContext.Personaje'  is null.");
+          }
+            _context.Personajes.Add(personaje);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPersonaje", new { id = personaje.Id }, personaje);
+        }
+
+        // DELETE: api/Personajes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePersonaje(int id)
+        {
+            if (_context.Personajes == null)
+            {
+                return NotFound();
+            }
+            var personaje = await _context.Personajes.FindAsync(id);
+            if (personaje == null)
+            {
+                return NotFound();
+            }
+
+            _context.Personajes.Remove(personaje);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PersonajeExists(int id)
+        {
+            return (_context.Personajes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
